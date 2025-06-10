031bd190f164ddea1934da937d8f42c01bffa6a7
jacobbailey8 <jbaileydsi87@gmail.com>
2025-06-10 17:26:11 -0400

    fill missing dates hourly on time series creation

--- app/model.py
+++ app/model.py
@@ -15,14 +15,14 @@ def load_model():
 def predict_pedocs(target_df, feature_df):
     model = load_model()
 
-    feature_series = TimeSeries.from_dataframe(feature_df, time_col='timestamp')
+    feature_series = TimeSeries.from_dataframe(feature_df, time_col='timestamp', freq='h', fill_missing_dates=True)
     past_covariates = feature_series[['pedocs_score_rolling_3h',
                          'pedocs_score_rolling_6h', 'pedocs_score_rolling_12h', 'pedocs_score_rolling_24h',
                            'pedocs_score_rolling_48h']]
     future_covariates = feature_series[['is_peak_hour', 'temperature_2m (°C)', 'wind_speed_10m (km/h)', 'is_valley_hour', 'is_weekend']]
 
 
-    target_series = TimeSeries.from_dataframe(target_df, time_col='timestamp')
+    target_series = TimeSeries.from_dataframe(target_df, time_col='timestamp', freq='h', fill_missing_dates=True)
     target = target_series['pedocs_score']
 
     return model.predict(n=24, series=target, past_covariates=past_covariates, future_covariates=future_covariates)
